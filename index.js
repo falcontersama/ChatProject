16ff572928294a85d398185d49022fdf3b6c6692
Roneaw <ronun15_6c-crazy@hotmail.com>
2018-04-14 03:13:09 +0700

    add various feature
    - message format
    - server room state
    - username feature

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var servermsg ={'default' : ['hi','hihi']};
+var servermsg ={'default' : []};
+var serverroom =['default'];
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    var roomname = 'default';
-    var clientmsg ={"default":[]};
-    socket.join(roomname);
-    io.to(socket.id).emit('update', servermsg[roomname]);
-    socket.on('join',function(newroom){
+    socket.roomname = 'default';
+    socket.clientmsg = {"default":[]};
+    socket.username = socket.id;
+    socket.join(socket.roomname);
+    io.to(socket.id).emit('update chat', servermsg[socket.roomname]);
+    io.to(socket.id).emit('update room', serverroom);
+    socket.on('join',(newroom)=>{
         socket.join(newroom);
-        roomname = newroom;
-        if (servermsg[roomname]==undefined){
-            servermsg[roomname] = [];
+        socket.roomname = newroom;
+        if (servermsg[socket.roomname]==undefined){
+            servermsg[socket.roomname] = [];
         }
-        clientmsg[roomname] = servermsg.roomname;
-        io.to(socket.id).emit('update',servermsg.roomname);
+        socket.clientmsg[socket.roomname] = servermsg[socket.roomname];
+        io.to(socket.id).emit('update chat',servermsg[socket.roomname]);
     })
+
+    socket.on('set username',(name)=>{
+        socket.username = name;
+    })
+
+    socket.on('add room',(room)=>{
+        serverroom.push(room);
+        io.emit('update room',serverroom);
+    })
+
     socket.on('chat message', function(msg){
-        servermsg[roomname].push(msg);
-        io.to(roomname).emit('update', servermsg[roomname]);
+        var d = new Date();
+        servermsg[socket.roomname].push({id: servermsg[socket.roomname].length +1, user : socket.username , message: msg , time: d.getHours()+ " : " + d.getMinutes() + " : "+ d.getSeconds()});
+        //for (id in io.sockets){
+            
+        //}
+        io.to(socket.roomname).emit('update chat', servermsg[socket.roomname]);
     });
+
   });
 
 http.listen(3000, function(){
