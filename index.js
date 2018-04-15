aa652308ae0b493cc076736c0d83739cfe8a41a5
Roneaw <ronun15_6c-crazy@hotmail.com>
2018-04-16 04:27:47 +0700

    add leave function and
    now support multiple client :D

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     socket.roomname = 'default';
     socket.clientmsg={};
-    socket.clientmsg[socket.roomname] = servermsg[socket.roomname].slice(0);
+    socket.clientmsg[socket.roomname] = [];
+    socket.msgcheckpoint ={};
+    socket.msgcheckpoint[socket.roomname] = servermsg[socket.roomname].length-1;
     socket.username = socket.id;
     socket.break ={'default': false};
     socket.join(socket.roomname);
@@ -26,10 +28,18 @@ io.on('connection', function(socket){
         if (servermsg[socket.roomname]==undefined){
             servermsg[socket.roomname] = [];
         }
-        console.log(socket.break[socket.roomname])
+        if(socket.msgcheckpoint[socket.roomname] == undefined){
+            socket.msgcheckpoint[socket.roomname] = servermsg[socket.roomname].length-1;
+        }
         if (!socket.break[socket.roomname]){
             socket.break[socket.roomname] = false;
-            socket.clientmsg[socket.roomname] = servermsg[socket.roomname].slice(0);
+            socket.clientmsg[socket.roomname] = [];
+            for(i=0; i<servermsg[socket.roomname].length;i++){
+                let m = servermsg[socket.roomname][i];
+                if(m.id>socket.msgcheckpoint[socket.roomname]){
+                    socket.clientmsg[socket.roomname].push(m);
+                }
+            }
         }
         io.to(socket.id).emit('update chat',socket.clientmsg[socket.roomname]);
     });
@@ -46,28 +56,49 @@ io.on('connection', function(socket){
     socket.on('break room',(room)=>{
         socket.break[room] = !socket.break[room];
         if (!socket.break[room]){
-            socket.clientmsg[room] = servermsg[room].slice(0);
-            if(room = socket.roomname){
+            socket.clientmsg[room] = [];
+            for(m in servermsg[room]){
+                if(m.id>socket.msgcheckpoint[room]){
+                    socket.clientmsg[room].push(m);
+                }
+            }
+            if(room == socket.roomname){
                 io.to(socket.id).emit('update chat', socket.clientmsg[room]);
             } 
         } 
     });
 
-    socket.on('chat message', function(msg){
+    socket.on('chat message', (msg)=>{
         var d = new Date();
-        servermsg[socket.roomname].push({id: servermsg[socket.roomname].length +1, user : socket.username , message: msg , time: d.getHours()+ " : " + d.getMinutes() + " : "+ d.getSeconds()});
+        servermsg[socket.roomname].push({
+             id: servermsg[socket.roomname].length,
+             user : socket.username ,
+             message: msg ,
+             time: d.getHours()+ ":" + d.getMinutes() + ":"+ d.getSeconds()});
         for (cid in io.sockets.sockets){
             csocket = io.sockets.sockets[cid]
             if(!csocket.break[socket.roomname] && csocket.rooms[socket.roomname]){
-                csocket.clientmsg[socket.roomname].push({id: servermsg[socket.roomname].length +1, user : socket.username , message: msg , time: d.getHours()+ " : " + d.getMinutes() + " : "+ d.getSeconds()})
-                io.to(csocket.id).emit('update chat',csocket.clientmsg[socket.roomname]);
+                csocket.clientmsg[socket.roomname].push({
+                    id: servermsg[socket.roomname].length,
+                    user : socket.username ,
+                    message: msg ,
+                    time: d.getHours()+ ":" + d.getMinutes() + ":"+ d.getSeconds()})
+                if(csocket.roomname == socket.roomname){
+                    io.to(csocket.id).emit('update chat',csocket.clientmsg[socket.roomname]);
+                }
             }
         }
     });
+
+    socket.on('leave',(room)=>{
+        delete socket.clientmsg[room];
+        delete socket.msgcheckpoint[room];
+        socket.leave(room);
+    })
 		var destination = 'localhost:3001';
 		//socket.emit('redirect', destination);
   });
 
 http.listen(Number(port), function(){
-    console.log('started on port '+port);
+    console.log('started on port ' + port);
 });
